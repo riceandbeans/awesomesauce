8ccc718b1dcbfb4e34f2a0ef450e270aa17257db
Michael Kovacs <kovacs@gmail.com>
2012-04-04 18:26:22 -0700

    add initial page. updated code

--- game.js
+++ game.js
@@ -1,4 +1,19 @@
 // -----------------------------------
 // aksjdflkasdf
 
-var games = ['kastbrian' 'MUAHAHAHAHAHAHAHAHAHAHAHAHAHAH!']
+var users = ['john', 
+             'bill'];
+
+var phone_book = {'john' : '415-232-1233', 
+                  'bill' : '415-231-1232'};
+
+var alphabetSounds  = {},
+    alphabet        = 'abcdefghijklmnopqrstuvwxyz'.split( '' );
+
+for( var i in alphabet ) {
+    var letter = alphabet[ i ];
+    alphabetSounds[ letter ] = letter;
+//new buzz.sound('sounds/kid/'+ letter );
+}
+
+// [1,2,3,4,5]
